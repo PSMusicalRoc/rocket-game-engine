a1913f3165af059596174c69b1fd9081f6121584
Tim Bishop <tjb9748@rit.edu>
2021-10-09 10:07:42 -0400

    Added documentation to GravityComponent.hpp and moved update_gravity to the GC file.

--- include/Physics/GravityComponent.hpp
+++ include/Physics/GravityComponent.hpp
@@ -1,14 +1,54 @@
+/**
+ * @file GravityComponent.hpp
+ * 
+ * This file holds the definition of the
+ * GravityComponent class. This is one of the physics
+ * components in the engine.
+ */
+
 #pragma once
 #include "Common/Entity.hpp"
 #include "Common/EntCompFunctions.hpp"
 
+/**
+ * This class adds gravity functionality to whatever Entity
+ * it's attached to. In simple terms, every frame it stores
+ * a new velocity in itself, and changes the Entity's internal
+ * positional attributes based on the velocity. Therefore, the
+ * Entity itself if moving downward, not specifically just
+ * the GravityComponent, as it acts directly on the Entity's
+ * actual position.
+ * 
+ * @brief A component adding gravity functionality to an
+ * Entity.
+ * 
+ * @class Yes
+ */
 struct GravityComponent
 {
+    /** @brief The current velocity of the gravity component.
+     * Updated every frame.
+     */
     float current_velocity = 0.0f;
 
+    /** @brief The contant of gravitational acceleration. */
     float gravity_constant;
 
+    /**
+     * Use this in tandem with add_component_to_entity().
+     * 
+     * @brief Constructor for GravityComponent.
+     * 
+     * @param gravity_constant The value to set the internal
+     * GravityCompoent::gravity_constant to. [default=9.81]
+     */
     GravityComponent(float gravity_constant = 9.81f);
 
-    
+    /**
+     * This is a function called every frame in PhysicsSystem.cpp.
+     * It is not meant to be called outside of that context.
+     * 
+     * @brief Updates a GravityComponent's velocity attribute.
+     */
+    static void update_gravity(GravityComponent* component, double deltatime);
 };
--- src/Physics/GravityComponent.cpp
+++ src/Physics/GravityComponent.cpp
@@ -2,3 +2,9 @@
 
 GravityComponent::GravityComponent(float gravity_constant)
     :gravity_constant(gravity_constant) {}
+
+
+void GravityComponent::update_gravity(GravityComponent* component, double deltatime)
+{
+    component->current_velocity = component->current_velocity + component->gravity_constant * deltatime;
+}
