d44760ad937173467806afac39d29ce82152587b
Tim Bishop <tjb9748@rit.edu>
2021-10-09 09:34:10 -0400

    Added documentation for Globals.hpp

--- include/Common/Globals.hpp
+++ include/Common/Globals.hpp
@@ -1,15 +1,29 @@
+/**
+ * @file Globals.hpp
+ * 
+ * This defines a bunch of global values used all over
+ * the program. Included here are things like ::screen_width
+ * and ::screen_height, as well as the default logging level,
+ * the target FPS, and other stuff.
+ */
+
 #pragma once
 
 struct Globals
 {
+    /** @brief The width of the window */
     static int screen_width;
+    /** @brief The height of the window */
     static int screen_height;
 
+    /** @brief The lowest log level to display, used in the Logger class */
     static int log_level;
 
+    /** @brief The target FPS for the game to run at. */
     static int FPS;
 };
 
+/** @brief The enum defining the logging levels. */
 enum LogLevel {
     INFO = 0,
     WARNING,
