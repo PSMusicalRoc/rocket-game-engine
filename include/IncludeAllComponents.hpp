cef18a4d9a4d85c5769d0426bccd069af3e8bd05
Tim Bishop <tjb9748@rit.edu>
2021-10-17 16:06:28 -0400

    Added documentation for init.hpp and IncludeAllComponents.hpp

--- include/IncludeAllComponents.hpp
+++ include/IncludeAllComponents.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file IncludeAllComponents.hpp
+ * 
+ * This is a shortcut include file that,
+ * when included, includes every single
+ * system and component implemented in the
+ * project. This is solely a cleanliness
+ * file.
+ */
+
 #pragma once
 
 #include "Physics/PhysicsSystem.hpp"
--- include/init.hpp
+++ include/init.hpp
@@ -1,3 +1,16 @@
+/**
+ * @file init.hpp
+ * 
+ * This file defines init(), which initializes
+ * some test entities. This is a file which will
+ * probably be replaced with many files (or even
+ * a mini little language) that initializes an
+ * entity when called/imported/whatever.
+ * 
+ * @note This will get deprecated when I come up
+ * with a better way to initialize specific entities
+ */
+
 #pragma once
 
 #include <SDL2/SDL.h>
@@ -7,4 +20,9 @@
 
 #include "IncludeAllComponents.hpp"
 
+/**
+ * @brief Initializes some test entities
+ * 
+ * @note Will be deprecated soon
+ */
 void init();
